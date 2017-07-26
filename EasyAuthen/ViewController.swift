309b240cfb52ca0e0d154ef3b87b7efce6b42ceb
Com06 <com06@Com06s-iMac.local>
2017-07-26 16:57:06 +0700

    NOT OK

--- EasyAuthen/ViewController.swift
+++ EasyAuthen/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Imlicit
+    let strDicUser = ["Sekitori":"12345", "tools":"321654"]
+    
+    @IBOutlet weak var UserTextFiel: UITextField!
+    
+    
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    
+    @IBOutlet weak var messageLebel: UILabel!
+    
+    
+    @IBAction func LoginButtom(_ sender: Any) {
+        
+     //get value form TextFirld
+        
+        let strUser: String = UserTextFiel.text!
+        let strPassword: String = PasswordTextField.text!
+        
+        print("User ==> \(strUser)")
+        print("Password ==> \(strPassword)")
+        
+        //Chack User 
+        if let strMyPassword = strDicUser[strUser] {
+            print("User OK")
+        }else{
+            print("User False")
+        }
+        
+        
+        
+    }//Login Buttom
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
